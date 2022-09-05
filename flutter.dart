fdc1eb3f54289a399deb5f543f53011d5fcc7df0
Induwara Rajapaksha <37555703+realrajapaksha@users.noreply.github.com>
2022-09-05 12:41:24 +0530

    Update flutter.dart

--- flutter.dart
+++ flutter.dart
@@ -2,8 +2,12 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 
-WidgetsFlutterBinding.ensureInitialized();
-await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp())
+}
+
 
 
 import 'package:firebase_auth/firebase_auth.dart';
